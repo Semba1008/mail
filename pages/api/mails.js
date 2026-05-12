d4f879423012ab523384a430c96f1ca61cc2a338
mahiro770 <mahiro.427.r9@gmail.com>
2026-05-12 13:09:06 +0900

    mails.js修正

--- pages/api/mails.js
+++ pages/api/mails.js
@@ -1,8 +1,9 @@
 import { supabase } from '../../lib/supabase'
 
 export default async function handler(req, res) {
+  // テーブル名を 'mails' から 'projects' に変更
   const { data, error } = await supabase
-    .from('mails')
+    .from('projects') 
     .select('*')
 
   res.status(200).json({ data, error })

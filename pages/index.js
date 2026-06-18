6998976d4854444882c123568906920eb3c0b7d1
mahiro770 <mahiro.427.r9@gmail.com>
2026-06-18 13:26:44 +0900

    修正

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
+import { supabaseAdmin } from "../lib/supabaseAdmin";
 
 const LINK_STYLE = { color: "#3182ce", textDecoration: "underline" };
 // 1ページあたりの表示件数
